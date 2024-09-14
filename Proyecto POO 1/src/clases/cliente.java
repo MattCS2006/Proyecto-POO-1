5d49a771bc1d8fd572554a23282469b01b7319f0
Matthew Cordero <masanroje06@gmail.com>
2024-09-14 11:49:23 -0600

    Base para clientes, con todos sus atributos, setters, getters y ambos constructores

--- Proyecto POO 1/src/clases/cliente.java
+++ Proyecto POO 1/src/clases/cliente.java
@@ -4,10 +4,94 @@
  */
 package clases;
 
+import java.util.Calendar;
+
 /**
  *
- * @author Rafael
+ * @author Matthew
  */
 public class cliente {
-    
+
+    public enum tipo_sexo {
+        MASCULINO, FEMENINO, NOESPECIFICADO
+    }
+
+    private int id;
+    private String nombre;
+    private String correo;
+    private String telefono;
+    private String direccion;
+    private tipo_sexo sexo;
+    private Calendar fechaNacimiento;
+
+    // Constructores
+    public cliente() {
+    }
+
+    public cliente(int id, String nombre, String correo, String telefono, String direccion, tipo_sexo sexo, Calendar fechaNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.sexo = sexo;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public tipo_sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(tipo_sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public Calendar getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Calendar fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
 }
