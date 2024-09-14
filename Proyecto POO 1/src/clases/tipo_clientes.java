eb3fead5b37215863d417d0281886d8fa916f0e3
Rafael Araya <143631166+RafaAraya14@users.noreply.github.com>
2024-09-14 10:43:51 -0600

    Add files via upload

---
+++ Proyecto POO 1/src/clases/tipo_clientes.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+/**
+ *
+ * @author Rafael
+ */
+public enum tipo_clientes {
+    NORMAL,PLATA, ORO
+}
